f84f45f830a487f4ebd7ac52da9e7910cabebf85
zhongzhiqiang <zqzhong1211@163.com>
2018-08-24 11:44:12 +0800

    feat: modify hashplugin to be more extensible and dependence modify.

--- HashPlugin.js
+++ HashPlugin.js
@@ -1,33 +1,58 @@
-const fs = require('fs');
+const fsExtra = require('fs-extra');
 const path = require('path');
 const pluginName = 'hash-plugin';
 const cwd = process.cwd();
+const defaultProcessor = hashMap => JSON.stringify(hashMap, null, 2);
+
 function HashPlugin(options = {}) {
-  this.options = options
+  this.options = options;
 }
 
-function collectHashMap(hashMap, asset, separator, pathRelative, reg) {
+function collectHashMap(hashMap, asset, separator, pathRelative, reg, prefix = '') {
   if (reg.test(asset)) {
-    hashMap[asset.split(separator)[0]] = `${pathRelative}/${asset}`;
+    hashMap[`${prefix}${asset.split(separator)[0]}`] = `${pathRelative}/${asset}`;
   }
 }
 
 // generate version config file
-HashPlugin.prototype.apply = function(compiler) {
-  const { separator = '_', outputPath = './server/config'} = this.options;
-  compiler.hooks.emit.tap(pluginName, compilation => {
-    const { assets, outputOptions} = compilation;
-    const pathRelative = path.relative(__dirname, outputOptions.path);
-    const cssHashMap = {}, jsHashMap = {};
-    const jsReg = /^js\//, cssReg = /^css\//;
-    Object.keys(assets).map(asset => {
-      collectHashMap(cssHashMap, asset, separator, pathRelative, cssReg);
-      collectHashMap(jsHashMap, asset, separator, pathRelative, jsReg);
+HashPlugin.prototype.apply = function (compiler) {
+  const {
+    separator = '_',
+    outputPath = './server/config',
+    isMerge = false,
+    filename = 'version',
+    extension = 'json',
+    processor = defaultProcessor,
+    prefix
+  } = this.options;
+  compiler
+    .hooks
+    .emit
+    .tap(pluginName, compilation => {
+      const {assets, outputOptions} = compilation;
+      const pathRelative = path.relative(__dirname, outputOptions.path);
+      const cssHashMap = {};
+      const jsHashMap = {};
+      const jsReg = /^js\//;
+      const cssReg = /^css\//;
+      Object
+        .keys(assets)
+        .map(asset => {
+          collectHashMap(cssHashMap, asset, separator, pathRelative, cssReg, prefix);
+          collectHashMap(jsHashMap, asset, separator, pathRelative, jsReg, prefix);
+        });
+      if (isMerge) {
+        const hashMap = {
+          ...cssHashMap,
+          ...jsHashMap
+        };
+        fsExtra.outputFileSync(path.resolve(cwd, outputPath, `${filename}.${extension}`), processor(hashMap));
+      } else {
+        fsExtra.outputFileSync(path.resolve(cwd, outputPath, `css_${filename}.${extension}`), processor(cssHashMap));
+        fsExtra.outputFileSync(path.resolve(cwd, outputPath, `js_${filename}.${extension}`), processor(jsHashMap));
+      }
+      console.info('---> Version file is generated ~');
     });
-    fs.writeFileSync(path.resolve(cwd, outputPath, './css_version.json'), JSON.stringify(cssHashMap, null, 2));
-    fs.writeFileSync(path.resolve(cwd, outputPath, './js_version.json'), JSON.stringify(jsHashMap, null, 2));
-    console.info('---> Version file is generated ~');
-  });
 };
 
 module.exports = HashPlugin;
